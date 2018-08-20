e8767a898f0151bd12468cfa087b9ea95610c52d
Jeanna <jeannamkruse@gmail.com>
2018-08-20 19:12:04 -0400

    adding faker, identifying fake usersand adding fake users to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+puts "Generating my user"
 u = User.find_or_create_by(username: "jruse")
 u.update_attributes({
   first_name: "Jeanna",
@@ -5,6 +6,26 @@ u.update_attributes({
   city:       "Columbus",
   state:      "OH",
   bio:        "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed id est laborum.",
+  fake:       false,
 })
 
 u.save!
+
+puts "Deleting fake users"
+User.where(fake: true). destroy_all
+
+puts "Generating fake users\n"
+10.times do
+  putc "."
+  User.create!({
+    username:    Faker::Internet.username,
+    first_name:  Faker::Name.first_name,
+    last_name:   Faker::Name.last_name,
+    city:        Faker::Address.city,
+    state:       Faker::Address.state_abbr,
+    bio:         Faker::Lorem.paragraph,
+    fake:        true,
+  })
+
+end
+puts "done"
